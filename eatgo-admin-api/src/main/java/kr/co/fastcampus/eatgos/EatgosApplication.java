df24ba21195df5318afb96b48a43154dc9afa9c5
doyeon.kim0208@gmail.com <doyeon.kim0208@gmail.com>
2020-03-13 23:08:51 +0900

    20200313 원상복귀시키는데 하루....

---
+++ eatgo-admin-api/src/main/java/kr/co/fastcampus/eatgos/EatgosApplication.java
@@ -0,0 +1,13 @@
+package kr.co.fastcampus.eatgos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EatgosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EatgosApplication.class, args);
+	}
+
+}
