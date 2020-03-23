1396832fb68660f4c841b74b61801d89a9d612c2
doyeon.kim0208@gmail.com <doyeon.kim0208@gmail.com>
2020-03-23 23:27:31 +0900

    200323  - user

--- eatgo-admin-api/src/main/java/kr/co/fastcampus/eatgos/EatgosApplication.java
+++ eatgo-admin-api/src/main/java/kr/co/fastcampus/eatgos/EatgosApplication.java
@@ -3,6 +3,7 @@ package kr.co.fastcampus.eatgos;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class EatgosApplication {
 
